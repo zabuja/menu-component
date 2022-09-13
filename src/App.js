b21cac663d97afdee38449877dbcd71a19ee352f
zabuja <atanu.tmsl@gmail.com>
2022-09-13 20:42:54 +0530

    picture updated menu

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
+  const[menuItems, setMenuItems] = useState(items);
+  const[categories, setCategories] = useState([]);
+
   return (
-   
+   <main>
+    <section className='menu section'>
+      <div className='title'>
+        <h2>our menu</h2>
+        <div className='underline'></div>
+      </div>
+      <Categories/>
+      <Menu items={menuItems}/>
+    </section>
+   </main>
   );
 }
 
