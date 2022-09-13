31d444682b594c51db1e462feb29efc8c94b5af0
zabuja <atanu.tmsl@gmail.com>
2022-09-13 18:53:16 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  return (
+   
+  );
+}
+
+export default App;
